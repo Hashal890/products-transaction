0e46bfbead633a8329910209e217d12db8844b32
amolpradeshi <pardeshiharshal90@gmail.com>
2024-06-22 20:50:13 +0530

    checking backend cors related error

--- products-transaction-backend/index.js
+++ products-transaction-backend/index.js
@@ -6,16 +6,26 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+const allowedOrigins = [
+  "https://products-transaction-statistics-frontend.vercel.app",
+  "https://products-transaction-statistics-backend.vercel.app",
+];
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://products-transaction-statistics-frontend.vercel.app",
-    methods: ["GET"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
-app.use(cors());
 app.use("/", productRouter);
 
 app.listen(PORT, async () => {
