08640f41049abbd7cf66ba4d206137aee9db7aac
amolpradeshi <pardeshiharshal90@gmail.com>
2024-06-22 13:53:17 +0530

    checking backend errors

--- products-transaction-frontend/src/App.jsx
+++ products-transaction-frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@chakra-ui/react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://products-transaction-statistics-backend.vercel.app/transactions"
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return <Box>Products Transaction</Box>;
 }
 
